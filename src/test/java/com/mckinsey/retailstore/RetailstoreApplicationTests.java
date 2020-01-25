61aca013aee9a82a1d306c663d7cf1122c1aaa9e
Saiyam <saiyam.123@gmail.com>
2020-01-25 14:21:41 +0530

    first commit

---
+++ src/test/java/com/mckinsey/retailstore/RetailstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mckinsey.retailstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
